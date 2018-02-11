19e94f8e8a32bfc87289edb28a5e6e6ad4887839
laosb <laosb@lasteam.net>
2018-02-11 15:21:17 +0800

    Initial Commit

---
+++ Laopad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Laopad
+//
+//  Created by Wexpo Lyu on 2018/2/11.
+//  Copyright © 2018年 Wexpo Lyu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Laopad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Laopad
+//
+//  Created by Wexpo Lyu on 2018/2/11.
+//  Copyright © 2018年 Wexpo Lyu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
